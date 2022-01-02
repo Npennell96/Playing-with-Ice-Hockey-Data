74ef3815ec995411d679f47493a6255300e5e399
Nick Pennell <njcp96@gmail.com>
2022-01-02 19:59:31 +1100

    more work

--- First_look.R
+++ First_look.R
@@ -36,7 +36,7 @@
 # Look ar roster
   # Column names
     roster %>% colnames()
-  # How do they lisy Postions?
+  # How do they list positions?
     roster$position %>% unique()
     # Poorly
   # How many and who plays 'D/RW'?
@@ -46,22 +46,52 @@
     roster[roster$player == 'Brent Burns', 'position'] <- 'D'
   # check
     roster[roster$player == 'Brent Burns', ]
-  # How many players in 2021 had the same name?
-    test <- roster %>% 
-      filter(player %in%
-        list(
-          roster %>% distinct(player, .keep_all = FALSE)
-        )
-      )
+  # Who are the forwards (f)?  
+    roster %>% filter(position == 'F')
+    # kind of weird, what makes them get F rather than positions, there hockeydb have them as potions. My guess would be the frequency
+    # at which they play across all the forward postions.
+    
+  # lets find the players that where traded during the year. They should be the ones to show up more than once
+    traded <- roster %>% get_dupes(player)
+  # but what if players share a name (we know this is the case from the sample code)
+    traded %>% group_by(player) %>% 
+      mutate(n_distinct = n_distinct(birth_date, height, weight)) %>% # changes of players having the same name, birth day, height and weight are very small
+      filter(n_distinct == 2) %>% select(!n_distinct)
+  # shows two players share the name Sebastian Aho, let remove it from the traded subset as if we didn't know the name
+  # or if there might be more
+    traded <- traded %>% filter(player != traded %>% group_by(player) %>% 
+                                          mutate(n_distinct = n_distinct(birth_date, height, weight)) %>% 
+                                          filter(n_distinct == 2) %>% select(player) %>% unique() %>% as.character()
+                        )
+  # was any one traded more than once? would show up more than twice.
+    traded %>% group_by(player) %>% count() %>% filter(n > 2) %>% select(player)
+  # Greg Pateryn, checek elite propects he had a busy travel year, https://www.eliteprospects.com/player/15526/greg-pateryn#transactions
     
     
-# I'm just going to look at the one game Edmonton (EDM) Vs Toronto (TOR) (Go Leafs Go!!)
+    
+# Now I'm just going to look at the one game Edmonton (EDM) Vs Toronto (TOR) (Go Leafs Go!!)
   # filter just take all events where home team is EDM 
     pbp_mini <- pbp %>% filter(home_abbreviation == 'EDM')
+  # and where players are just from EDM or TOR
+    teams <- c("EDM", "TOR")
+    roster_mini <- roster %>% filter(team_abbr %in% teams)
+  # how many players did each team use for the year?
+    roster_mini %>% group_by(team_name) %>% count()
+  # you can only use 20 on a given night (including a backup goalie who might not play). 
+  # So who where the players for the data we have?
+    # players
+      players <- pbp_mini %>% select(grep('home_on|away_on|home_goalie|away_goalie', pbp_mini %>% colnames(), value = TRUE)) %>%
+                  gather(value = 'player') %>% select(player) %>% unique() %>% na.omit()
+    # for fun lets join with position, team and number for roster mini
+      # format up roster_mini
+        roster_mini <- roster_mini %>% select(player, team_name, position, number)
     
     
-    
-    
-    
-    
-    
+   
+# traded and players of the same name /errors
+  test <- roster %>% get_dupes(player)
+  test <- test %>% group_by(player) %>% mutate(n_distinct = n_distinct(age, height, weight))
+  
+        
+        
+         
